e06ae9e119766617fe301757a914692abca256ab
Brandon0230 <103169140+Brandon0230@users.noreply.github.com>
2025-09-13 21:23:06 +1000

    Add files via upload

---
+++ Models/Tasks.cs
@@ -0,0 +1,9 @@
+﻿namespace DeploymentTask3.Models
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+    }
+}
